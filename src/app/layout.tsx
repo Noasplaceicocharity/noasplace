de81e999186a60dcf7b51dd6bef98b51ae79ca0c
ChurchAcademy <hello@churchacademy.co.uk>
2025-09-02 18:21:39 +0100

    meta pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Nunito, Inter } from "next/font/google";
 import ScrollToTop from "@/components/ScrollToTop";
 import "./globals.css";
@@ -81,6 +82,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1086670053658224');
+fbq('track', 'PageView');
+          `}
+        </Script>
         <script type="application/ld+json">
           {JSON.stringify({
             "@context": "https://schema.org",
@@ -129,6 +144,12 @@ export default function RootLayout({
         </script>
       </head>
       <body className={`${nunito.variable} ${inter.variable} antialiased bg-background text-foreground`}>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html:
+              '<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1086670053658224&ev=PageView&noscript=1" />',
+          }}
+        />
         <ScrollToTop />
         {children}
       </body>

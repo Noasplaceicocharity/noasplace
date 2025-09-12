a2019833d8bad08c9037a8b1401017573e6c708a
ChurchAcademy <hello@churchacademy.co.uk>
2025-09-12 23:27:59 +0100

    blog

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "s3.us-west-2.amazonaws.com" },
-      { protocol: "https", hostname: "prod-files-secure.s3.us-west-2.amazonaws.com" },
-      { protocol: "https", hostname: "www.notion.so" },
-      { protocol: "https", hostname: "notion.so" },
-      { protocol: "https", hostname: "prod-files-secure.s3.us-west-2.amazonaws.com" },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
